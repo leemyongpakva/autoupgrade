283e3d2120d1e2dc8d55e1fd0651cb69e6490135
Karlis Suvi <45097959+ks129@users.noreply.github.com>
2024-09-27 21:15:01 +0300

    Add feature value position field

--- upgrade/sql/9.0.0.sql
+++ upgrade/sql/9.0.0.sql
@@ -292,3 +292,7 @@ DELETE FROM `PREFIX_hook` WHERE `name` IN (
 /* Clean hook registrations related to removed hooks */
 DELETE FROM `PREFIX_hook_module` WHERE `id_hook` NOT IN (SELECT id_hook FROM `PREFIX_hook`);
 DELETE FROM `PREFIX_hook_module_exceptions` WHERE `id_hook` NOT IN (SELECT id_hook FROM `PREFIX_hook`);
+
+/* Feature value position */
+/* https://github.com/PrestaShop/PrestaShop/pull/37042 */
+/* PHP:add_column('feature_value', 'position', 'int(10) unsigned NOT NULL DEFAULT \'0\''); */;
