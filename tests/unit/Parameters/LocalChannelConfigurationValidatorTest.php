2c6579f8d65a669f0362a03bf1e61a469847266e
Alexis Guyomar <alexis.guyomar@prestashop.com>
2024-10-29 14:34:50 +0100

    feat: add prestashop version service tests

--- tests/unit/Parameters/LocalChannelConfigurationValidatorTest.php
+++ tests/unit/Parameters/LocalChannelConfigurationValidatorTest.php
@@ -13,10 +13,6 @@ class LocalChannelConfigurationValidatorTest extends TestCase
 
     protected function setUp(): void
     {
-        if (PHP_VERSION_ID >= 80000) {
-            $this->markTestSkipped('An issue with this version of PHPUnit and PHP 8+ prevents this test to run.');
-        }
-
         $this->container = new UpgradeContainer('/html', '/html/admin');
 
         $downloadPath = __DIR__ . '/../../fixtures/localChannel/';
