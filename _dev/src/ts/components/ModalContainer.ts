0b5ec253f9486e1474e4a3dde0836dc1dd570776
Thomas Nabord <thomas.nabord@prestashop.com>
2024-11-01 14:35:05 +0000

    Enable modal auto closing and event triggering

--- _dev/src/ts/components/ModalContainer.ts
+++ _dev/src/ts/components/ModalContainer.ts
@@ -1,12 +1,28 @@
 import Hydration from "../utils/Hydration";
 
 export default class ModalContainer {
+    public static cancelEvent = 'cancel';
+    public static okEvent = 'ok';
+
     public static containerId = 'ua_modal';
     public init(): void
     {
-        document.getElementById(ModalContainer.containerId)?.addEventListener(
-            Hydration.hydrationEventName, () => this.displayModal(),
-        );
+        const modalContainer = document.getElementById(ModalContainer.containerId);
+
+        if (modalContainer) {
+            modalContainer.addEventListener(
+                Hydration.hydrationEventName, () => this.displayModal(),
+            );
+            // Event delegation
+            modalContainer.addEventListener('click', (ev: Event) => {
+                const modal = ev.target?.closest(".modal");
+                if (modal && ev.target?.closest("[data-dismiss='modal']")) {
+                    modal.dispatchEvent(new Event(ModalContainer.cancelEvent));
+                } else if(modal && ev.target?.closest(".modal-footer button:not([data-dismiss='modal'])")) {
+                    modal.dispatchEvent(new Event(ModalContainer.okEvent))
+                }
+            });
+        }
     }
 
     private displayModal(): void {
@@ -15,6 +31,22 @@ export default class ModalContainer {
         ).forEach((modal: HTMLElement) => {
             modal.style.display = 'block';
             modal.classList.add('in');
+
+            modal.addEventListener(
+                ModalContainer.cancelEvent, this.closeModal,
+            );
+            modal.addEventListener(
+                ModalContainer.okEvent, this.closeModal,
+            );
         });
-}
+    }
+
+    private closeModal(ev: Event): void {
+        const modal = ev.target;
+        if (modal) {
+            modal.classList.remove('in');
+            // Let the animation play before hiding the modal
+            setTimeout(() => modal.style.display = null, 500);
+        }
+    }
 }
