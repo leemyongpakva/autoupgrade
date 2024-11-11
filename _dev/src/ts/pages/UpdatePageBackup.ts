4443df9bb93e5c83ee4ba91234fa7a2db30c3c4f
Thomas Nabord <thomas.nabord@prestashop.com>
2024-11-11 14:48:04 +0000

    Refacto ts of backup page with event delegation

--- _dev/src/ts/pages/UpdatePageBackup.ts
+++ _dev/src/ts/pages/UpdatePageBackup.ts
@@ -4,20 +4,13 @@ import UpdatePage from './UpdatePage';
 
 export default class UpdatePageBackup extends UpdatePage {
   protected stepCode = 'backup';
-  protected form: HTMLFormElement;
   protected formFields: Element[] = [];
-
-  constructor() {
-    super();
-
-    this.form = this.assertAndGetForm();
-    
-  }
 
   public mount() {
     this.initStepper();
-    this.initFormFields(this.form);
     this.form.addEventListener('submit', this.onFormSubmit);
+    this.form.addEventListener('change', this.onInputChange);
+
     document.getElementById('ua_container')?.addEventListener('click', this.onClick);
     document.getElementById(ModalContainer.containerId)?.addEventListener(ModalContainer.okEvent, (ev) => {
       if (ev.target.id === 'modal-confirm-backup') {
@@ -30,12 +23,10 @@ export default class UpdatePageBackup extends UpdatePage {
 
   public beforeDestroy() {
     this.form.removeEventListener('submit', this.onFormSubmit);
-    this.formFields.forEach((element) => {
-      element.removeEventListener('change', this.onInputChange);
-    });
+    this.form.removeEventListener('change', this.onInputChange);
   }
 
-  private assertAndGetForm(): HTMLFormElement {
+  private get form(): HTMLFormElement {
     const form = document.forms.namedItem('update-backup-page-form');
     if (!form) {
       throw new Error('Form not found');
@@ -50,27 +41,15 @@ export default class UpdatePageBackup extends UpdatePage {
     return form;
   }
 
-  private initFormFields(form: HTMLFormElement): void {
-    Array.from(form.elements).forEach((element) => {
-      if (element.nodeName !== 'INPUT') {
-        return;
-      }
-
-      this.formFields.push(element);
-
-      element.addEventListener('change', this.onInputChange);
-    });
-  }
-
-  private onClick = (ev: Event) => {
+  private onClick = async (ev: Event) => {
     if (ev.target.id === 'update-backup-page-skip-btn') {
       const formData = new FormData();
       formData.append('backupDone', JSON.stringify(false));
-      api.post(this.form.dataset.routeToSubmitUpdate!, formData);
+      await api.post(this.form.dataset.routeToSubmitUpdate!, formData);
     }
   };
 
-  private onInputChange = (ev: Event) => {
+  private onInputChange = async (ev: Event) => {
     const optionInput = ev.target as HTMLInputElement;
 
     optionInput.setAttribute('disabled', 'true');
@@ -78,14 +57,14 @@ export default class UpdatePageBackup extends UpdatePage {
     const data = new FormData();
     data.append('name', optionInput.name);
     data.append('value', JSON.stringify(optionInput.checked));
-    api.post(this.form.dataset.routeToSave!, data);
+    await api.post(this.form.dataset.routeToSave!, data);
 
     optionInput.removeAttribute('disabled');
   };
 
-  private onFormSubmit = (event: Event) => {
+  private onFormSubmit = async (event: Event) => {
     event.preventDefault();
 
-    api.post(this.form.dataset.routeToSubmitBackup!, new FormData(this.form));
+    await api.post(this.form.dataset.routeToSubmitBackup!, new FormData(this.form));
   };
 }
