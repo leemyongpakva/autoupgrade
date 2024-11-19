e3275b3a195f6f67a70b004efc0bf4eb1f34f516
Alexis Guyomar <alexis.guyomar@prestashop.com>
2024-11-19 10:16:53 +0100

    fxi: remove dev code

--- _dev/src/ts/pages/UpdatePageUpdate.ts
+++ _dev/src/ts/pages/UpdatePageUpdate.ts
@@ -1,7 +1,4 @@
 import UpdatePage from './UpdatePage';
-import ProgressBar from '../components/ProgressBar';
-import LogsSummary from '../components/LogsSummary';
-import LogsViewer from '../components/LogsViewer';
 
 export default class UpdatePageUpdate extends UpdatePage {
   protected stepCode = 'update';
@@ -12,20 +9,5 @@ export default class UpdatePageUpdate extends UpdatePage {
 
   public mount() {
     this.initStepper();
-
-    // uncomment for dev purpose
-
-    const progressBarContainer = document.querySelector(
-      '[data-component="progress-bar"]'
-    ) as HTMLElement;
-    window.ProgressBar = new ProgressBar(progressBarContainer!);
-    const logsSummaryContainer = document.querySelector(
-      '[data-component="logs-summary"]'
-    ) as HTMLElement;
-    window.LogsSummary = new LogsSummary(logsSummaryContainer!);
-    const logsViewerContainer = document.querySelector(
-      '[data-component="logs-viewer"]'
-    ) as HTMLElement;
-    window.LogsViewer = new LogsViewer(logsViewerContainer);
   }
 }
